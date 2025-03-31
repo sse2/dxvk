7ca856de8256b543c4b96954d49f95d46b1c9c2c
sse2 <readfsdword0x30@gmail.com>
2025-03-31 22:28:20 +0300

    [vulkan] load libMoltenVK on macOS

--- src/vulkan/vulkan_loader.cpp
+++ src/vulkan/vulkan_loader.cpp
@@ -7,20 +7,25 @@
 #include "../util/util_string.h"
 #include "../util/util_win32_compat.h"
 
-namespace dxvk::vk {
+namespace dxvk::vk
+{
 
-  static std::pair<HMODULE, PFN_vkGetInstanceProcAddr> loadVulkanLibrary() {
-    static const std::array<const char*, 2> dllNames = {{
+  static std::pair<HMODULE, PFN_vkGetInstanceProcAddr> loadVulkanLibrary()
+  {
+    static const std::array<const char *, 2> dllNames = {{
 #ifdef _WIN32
-      "winevulkan.dll",
-      "vulkan-1.dll",
+        "winevulkan.dll",
+        "vulkan-1.dll",
+#elif defined(__APPLE__)
+        "libMoltenVK.dylib",
 #else
-      "libvulkan.so",
-      "libvulkan.so.1",
+        "libvulkan.so",
+        "libvulkan.so.1",
 #endif
     }};
 
-    for (auto dllName : dllNames) {
+    for (auto dllName : dllNames)
+    {
       HMODULE library = LoadLibraryA(dllName);
 
       if (!library)
@@ -28,7 +33,8 @@ namespace dxvk::vk {
 
       auto proc = GetProcAddress(library, "vkGetInstanceProcAddr");
 
-      if (!proc) {
+      if (!proc)
+      {
         FreeLibrary(library);
         continue;
       }
@@ -38,75 +44,77 @@ namespace dxvk::vk {
     }
 
     Logger::err("Vulkan: vkGetInstanceProcAddr not found");
-    return { };
+    return {};
   }
 
-  LibraryLoader::LibraryLoader() {
+  LibraryLoader::LibraryLoader()
+  {
     std::tie(m_library, m_getInstanceProcAddr) = loadVulkanLibrary();
   }
 
-  LibraryLoader::LibraryLoader(PFN_vkGetInstanceProcAddr loaderProc) {
+  LibraryLoader::LibraryLoader(PFN_vkGetInstanceProcAddr loaderProc)
+  {
     m_getInstanceProcAddr = loaderProc;
   }
 
-  LibraryLoader::~LibraryLoader() {
+  LibraryLoader::~LibraryLoader()
+  {
     if (m_library)
       FreeLibrary(m_library);
   }
 
-  PFN_vkVoidFunction LibraryLoader::sym(VkInstance instance, const char* name) const {
+  PFN_vkVoidFunction LibraryLoader::sym(VkInstance instance, const char *name) const
+  {
     return m_getInstanceProcAddr(instance, name);
   }
 
-  PFN_vkVoidFunction LibraryLoader::sym(const char* name) const {
+  PFN_vkVoidFunction LibraryLoader::sym(const char *name) const
+  {
     return sym(nullptr, name);
   }
 
-  bool LibraryLoader::valid() const {
+  bool LibraryLoader::valid() const
+  {
     return m_getInstanceProcAddr != nullptr;
   }
-  
-  
-  InstanceLoader::InstanceLoader(const Rc<LibraryLoader>& library, bool owned, VkInstance instance)
-  : m_library(library), m_instance(instance), m_owned(owned) { }
-  
-  
-  PFN_vkVoidFunction InstanceLoader::sym(const char* name) const {
+
+  InstanceLoader::InstanceLoader(const Rc<LibraryLoader> &library, bool owned, VkInstance instance)
+      : m_library(library), m_instance(instance), m_owned(owned) {}
+
+  PFN_vkVoidFunction InstanceLoader::sym(const char *name) const
+  {
     return m_library->sym(m_instance, name);
   }
-  
-  
-  DeviceLoader::DeviceLoader(const Rc<InstanceLoader>& library, bool owned, VkDevice device)
-  : m_library(library)
-  , m_getDeviceProcAddr(reinterpret_cast<PFN_vkGetDeviceProcAddr>(
-      m_library->sym("vkGetDeviceProcAddr"))),
-    m_device(device), m_owned(owned) { }
-  
-  
-  PFN_vkVoidFunction DeviceLoader::sym(const char* name) const {
+
+  DeviceLoader::DeviceLoader(const Rc<InstanceLoader> &library, bool owned, VkDevice device)
+      : m_library(library), m_getDeviceProcAddr(reinterpret_cast<PFN_vkGetDeviceProcAddr>(
+                                m_library->sym("vkGetDeviceProcAddr"))),
+        m_device(device), m_owned(owned) {}
+
+  PFN_vkVoidFunction DeviceLoader::sym(const char *name) const
+  {
     return m_getDeviceProcAddr(m_device, name);
   }
-  
-  
-  LibraryFn::LibraryFn() { }
+
+  LibraryFn::LibraryFn() {}
   LibraryFn::LibraryFn(PFN_vkGetInstanceProcAddr loaderProc)
-  : LibraryLoader(loaderProc) { }
-  LibraryFn::~LibraryFn() { }
-  
-  
-  InstanceFn::InstanceFn(const Rc<LibraryLoader>& library, bool owned, VkInstance instance)
-  : InstanceLoader(library, owned, instance) { }
-  InstanceFn::~InstanceFn() {
+      : LibraryLoader(loaderProc) {}
+  LibraryFn::~LibraryFn() {}
+
+  InstanceFn::InstanceFn(const Rc<LibraryLoader> &library, bool owned, VkInstance instance)
+      : InstanceLoader(library, owned, instance) {}
+  InstanceFn::~InstanceFn()
+  {
     if (m_owned)
       this->vkDestroyInstance(m_instance, nullptr);
   }
-  
-  
-  DeviceFn::DeviceFn(const Rc<InstanceLoader>& library, bool owned, VkDevice device)
-  : DeviceLoader(library, owned, device) { }
-  DeviceFn::~DeviceFn() {
+
+  DeviceFn::DeviceFn(const Rc<InstanceLoader> &library, bool owned, VkDevice device)
+      : DeviceLoader(library, owned, device) {}
+  DeviceFn::~DeviceFn()
+  {
     if (m_owned)
       this->vkDestroyDevice(m_device, nullptr);
   }
-  
+
 }
